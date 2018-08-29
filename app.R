08af7a3ffbd49d8b02f27f77fe1889f51e340db4
Jonathan Lin <jonathan.lin1138@live.com>
2018-08-29 11:46:27 -0700

    Finished helper functions in helper.R; implemented helper functions in app.R to generate graph based on user inputs

--- app.R
+++ app.R
@@ -11,19 +11,25 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       ## Dropdown menu to choose which type of area one wants to limit the area search by
-      selectInput("area_category", "Search By:", c("Zip Code","County", "City", "State", "Neighborhood", "Greater Metropolitan Area")),
+      selectInput("area_category", 
+                  "Search By:", 
+                  selected = "State",
+                  c("Zip Code","County", "City", "State", "Neighborhood", "Greater Metropolitan Area")),
       
       ## dropdown to choose what range of dates to use in displaying information
-      dateRangeInput("date_range", label = "Date range", start = "1996-04-30"),
+      dateRangeInput("date_range", 
+                     label = "Date range", 
+                     start = "1996-04-30"),
       
-      ## outputs the searchbar in the ui
-      uiOutput("searchbar2"),
+      ## outputs the specific area searchbar in the ui
+      uiOutput("searchbar"),
       
       ## dropdown to determine the category of information desired 
       selectInput("indicator",
                   label = "Desired Parameter",
                   c("Choose" = '',
-                    filtered_indicators$INDICATOR),
+                   filtered_indicators$INDICATOR),
+                  selected = "Zillow Home Value Index - All Homes",
                   selectize = TRUE)
 
       
@@ -43,17 +49,37 @@ server <- function(input, output, session) {
   #   geom_point(mapping = aes(x= data3$Date, y= data3$Value))
   # output$graph <- renderPlot(temp_graph)
   
-  output$graph <- ggplot
+  
   
   ## renders the search bar for choosing the specific area name 
-  output$searchbar2 <- renderUI(selectInput('area_input', 
-                                            'Enter Specific Area Name', 
+  output$searchbar <- renderUI(selectInput('area_input', 
+                                            'Enter Specific Area Name',
                                             c("Choose" ='',as.character(mappingFinder(input$area_category))), 
+                                            selected = 'Washington',
                                             selectize=TRUE)
-)
+                                            )
+  ## uses the codeBuilder function from helper.R to keep the Shiny app with an updated Quandl API call and
+  ## subsequent data frame
+  #current_code <- reactive({return(codeBuilder(input$indicator,input$area_category,input$area_input))})
+  #current_dataframe <- reactive({return(Quandl(current_code))})
   
+  current_dataframe <- reactive({
+    return(
+      filter(
+        Quandl(
+          codeBuilder(
+            input$indicator,input$area_category,input$area_input
+          )
+        )
+      , Date > input$date_range[1] && Date < input$date_range[2]
+      )
+    )
+  })
   
-  
+  output$graph <- renderPlot(
+    ggplot(data = current_dataframe()) +
+      geom_point(mapping = aes(x = current_dataframe()$Date, y = current_dataframe()$Value))
+  )
 
 }
 
--- helper.R
+++ helper.R
@@ -9,7 +9,7 @@ source('secret/api-key.R')
 Quandl.api_key(api_key)
 
 ## reads in various indicator codes to data frames
-unfiltered_indicators <- read.csv("indicators.csv")
+unfiltered_indicators <- read.csv("indicators.csv", stringsAsFactors = FALSE)
 state_mapping_code <- read.csv("filtered_state_mapping.csv")
 county_mapping_code <- read.csv("filtered_county_mapping.csv")
 city_mapping_code <- read.csv("filtered_city_mapping.csv")
@@ -36,8 +36,9 @@ filtered_indicators <- filteredIndicatorBuilder(desired_indicator_params)
 ## Takes in a string area category (county, state, city), and returns a factor of all possible choices in
 ## that category
 mappingFinder <- function(area_name) {
-  if (area_name == "City")
+  if (area_name == "City") {
     return(city_mapping_code$AREA)
+  }
   else if (area_name == "State") {
     return(state_mapping_code$AREA)
   }
@@ -52,12 +53,53 @@ mappingFinder <- function(area_name) {
   }
 }
 
+## takes in a user input string (name of state, county, or etc.) and returns the matching indicator code
+## using the mapping_code dataframes
+subCodeBuilderArea <- function(area_name,area_category) {
+  if (area_category == 'State') {
+    return(as.character(filter(state_mapping_code,AREA == area_name)$CODE))
+  }
+  else if (area_category == 'County') {
+    return(as.character(filter(county_mapping_code,AREA == area_name)$CODE))
+  }
+  else if (area_category == 'City') {
+    return(as.character(filter(city_mapping_code,AREA == area_name)$CODE))
+  }
+  else if (area_category == 'Greater Metropolitan Area') {
+    return(as.character(filter(greatermetro_mapping_code,AREA == area_name)$CODE))
+  }
+  else if (area_category == 'Neighborhood') {
+    return(as.character(filter(neighborhood_mapping_code,AREA == area_name)$CODE))
+  }
+  else {
+    return(area_name)
+  }
+}
+
 ## takes in a string of the specific choice of area, the string of the specific choice of search category, and
 ## returns the Quandl api code to get the data fitting those parameters
-codeBuilder <- function()
+codeBuilder <- function(indicator_name,area_category,area_name) {
+  area_abbrev <- 'Z'
+  if (area_category == 'State') {
+    area_abbrev = 'S'
+  } else if (area_category == "County") {
+    area_abbrev = "CO"
+  } else if (area_category == "City") {
+    area_abbrev = "C"
+  } else if (area_category == "Greater Metro Area") {
+    area_abbrev = "M"
+  } else if (area_category == "Neighborhood") {
+    area_abbrev = "N"
+  }
+  return(paste0('ZILLOW/',area_abbrev,subCodeBuilderArea(area_name,area_category),'_',subCodeBuilderIndicator(indicator_name)))
+}
 
+## takes in a string "title" of a certain indicator and returns a string of the matching indicator code
+subCodeBuilderIndicator <- function(indicator_name) {
+  return(as.character(filter(filtered_indicators,INDICATOR == indicator_name)$CODE))
+}
 
-data3 <- Quandl('ZILLOW/Z98006_MLPAH')
+data3 <- Quandl('ZILLOW/S3_ZHVIAH')
 filtered_data3 <- filter(data3, substring(Date,1,4) == '2018')
 data4 <- Quandl('ZILLOW/Z98006_PHDVAH')
 
